c8687328719c293b703ccbc9c0909fd012a1d0a6
integraldx <integraldx@integraldx.me>
2021-10-19 23:11:36 +0900

    Made settings system to reference json

--- Editor/UniAPISettings.cs
+++ Editor/UniAPISettings.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 namespace Integraldx.UniAPI.Editor
 {
     [CreateAssetMenu(fileName = "UniAPISettings", menuName = "UniAPI/Settings")]
-    public class UniAPISettings : ScriptableSingleton<UniAPISettings>
+    public class UniAPISettings : ScriptableObject
     {
         [SerializeField]
         [InspectorName("OpenAPI specification file")]
